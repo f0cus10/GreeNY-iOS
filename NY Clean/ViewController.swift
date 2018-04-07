26ba219f2fd4e091b59038147f8acc1a0440566c
joshuageronimo <joshuag31@hotmail.com>
2018-04-07 18:22:43 -0400

    Parse JSON data

--- NY Clean/ViewController.swift
+++ NY Clean/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
+struct RecycleBin: Decodable {
+    
+    let address: String?
+    let borough: String?
+    let latitude: String?
+    let longitude: String?
+    let park_site_name: String?
+    let site_type: String?
+    
+}
+
 class ViewController: UIViewController {
 
+    
+    let url = URL(string: "https://data.cityofnewyork.us/resource/ggvk-gyea.json?$$app_token=TsaFLCw8emTtCfbOt0MsufYue")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let session = URLSession.shared
+        let request = URLRequest(url: url!)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            do {
+                let json = try JSONDecoder().decode([RecycleBin].self, from: data!)
+                
+                for bins in json {
+                    print(bins.address ?? "no address")
+                }
+                
+            } catch let jsonError {
+                print("error, \(jsonError)")
+            }
+            
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
