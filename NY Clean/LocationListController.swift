17e71d52f3485d30eac2b9d475b5bda09f96f076
Iftikhar Khan <iakhan4425@gmail.com>
2018-04-07 18:56:35 -0400

    Updated with extra location file

---
+++ NY Clean/LocationListController.swift
@@ -0,0 +1,19 @@
+//
+//  LocationListController.swift
+//  NY Clean
+//
+//  Created by Iftikhar Khan on 4/7/18.
+//  Copyright © 2018 Joshua Geronimo. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class LocationListController: UIViewController, CLLocationManagerDelegate {
+  
+  var location: CLLocationManager!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
